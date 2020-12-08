88d29bce548244cedd0262f136534ed2d7e773bc
Guan Hua <409570200@gapp.fju.edu.tw>
2020-12-08 15:33:48 +0800

    Poker = 25

---
+++ src/edu/fju/Poker.java
@@ -0,0 +1,12 @@
+package edu.fju;
+
+public class Poker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			PokerCard card = new PokerCard(25);
+			System.out.println(card.value);
+			
+		}
+
+}
--- src/edu/fju/PokerCard.java
+++ src/edu/fju/PokerCard.java
@@ -1,5 +1,11 @@
 package edu.fju;
 
 public class PokerCard {
-
+		int value;
+		char type;
+		int point;
+		
+			public PokerCard(int value) {
+				this.value=value;
+			}
 }
